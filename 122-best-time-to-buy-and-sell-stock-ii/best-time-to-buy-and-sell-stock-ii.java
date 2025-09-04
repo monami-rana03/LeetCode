714b3ffafe94fe121a63f624a2d1c4af12f26e2f
Monami Rana <121621269+monami-rana03@users.noreply.github.com>
2025-09-04 15:38:37 +0530

    Time: 1 ms (77.48%) | Memory: 46.3 MB (14.73%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max_profit=0;
+        int buy_price=prices[0];
+        for(int i=1;i<prices.length;i++){
+            if(buy_price<prices[i]){
+                max_profit+=prices[i]-buy_price;
+                // max_profit=max_profit+(prices[i]-buy_price);
+                // buy_price=prices[i];
+            }
+            buy_price=prices[i];
+        }
+        return max_profit;
+    }
+}
+
+
+
+
+
+
+
+
+
+
