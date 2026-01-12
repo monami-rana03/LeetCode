13e3f1e9d04201a2b8a6c2c54fc451b2c9507ce0
Monami Rana <121621269+monami-rana03@users.noreply.github.com>
2026-01-12 15:01:43 +0530

    Time: 3 ms (16.62%) | Memory: 44.8 MB (46.58%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public List<String> generateParenthesis(int n) {
+
+        List<String> result = new ArrayList<>();
+        backtrack(result, "", 0, 0, n);
+        return result;
+    }
+
+    private void backtrack(List<String> result, String current,
+                           int open, int close, int n) {
+
+        // If the string length is 2*n, it is a valid combination
+        if (current.length() == 2 * n) {
+            result.add(current);
+            return;
+        }
+
+        // Add '(' if we still have opening brackets left
+        if (open < n) {
+            backtrack(result, current + "(", open + 1, close, n);
+        }
+
+        // Add ')' only if it won't make it invalid
+        if (close < open) {
+            backtrack(result, current + ")", open, close + 1, n);
+        }
+    }
+}
